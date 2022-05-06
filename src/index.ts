d06ed4982977435db096e4237fa4315af43d752d
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-05-06 17:00:37 -0300

    login

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import * as express from 'express'
 
+import { json, raw, text, urlencoded } from 'body-parser'
+
 const port = process.env.PORT
 
 const server = express()
+
+server.use(json())
+server.use(raw())
+server.use(text())
+server.use(urlencoded({ extended: true }))
 
 server.use(express.static('public'))
 
@@ -10,6 +17,16 @@ server.get('/app-id', (req, res) => {
   const appId = process.env.APP_ID
 
   res.status(200).send(appId)
+})
+
+server.all('/login', (req, res) => {
+  const headers = req.headers
+  const body = req.body
+
+  console.log(headers)
+  console.log(body)
+
+  res.status(200).send()
 })
 
 server.listen(port, () => {
